b8d1b6fd8d5ab32c8993b3f8fe9aae51b596dd9c
ulzirok <ulzirok@gmail.com>
2025-07-09 16:26:33 +0500

    add js logic - photo slider, video slider, videoPleer, scrollTop

---
+++ js/sliderImg.js
@@ -0,0 +1,79 @@
+const inner = document.querySelector('.welcome__slider-inner');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const liAll = document.querySelectorAll('.welcome__slider-indicators li');
+const currentSpan = document.getElementById('current');
+const totalSpan = document.getElementById('total');
+
+let move = 0;
+let i = 0;
+totalSpan.textContent = liAll.length.toString().padStart(2, '0');
+
+next.addEventListener('click', () => {
+  right();
+});
+prev.addEventListener('click', () => {
+  left();
+});
+
+function right() {
+
+  liAll[i].classList.remove('active');
+
+  i++;
+  if (i === liAll.length) {
+    i = 0;
+  }
+  liAll[i].classList.add('active');
+
+  move -= 1000;
+  if (move < -1000 * (liAll.length - 1)) {
+    move = 0;
+  }
+  
+  inner.style.left = move + 'px';
+  
+  updateCounter()
+}
+
+function left() {
+
+  liAll[i].classList.remove('active');
+  i--;
+  if (i < 0) {
+    i = liAll.length - 1;
+  }
+  liAll[i].classList.add('active');
+
+  if (move === 0) {
+    move = -1000 * (liAll.length - 1);
+  } else {
+    move += 1000;
+  }
+
+  updateCounter()
+}
+
+liAll.forEach((li, index) => {
+  li.addEventListener('click', function () {
+    liAll.forEach((li) => {
+      li.classList.remove('active');
+    });
+    
+    this.classList.add('active');
+    i = index
+    
+    move = -1000 * index;
+    inner.style.left = move + 'px';
+    
+    updateCounter()
+  });
+})
+
+function updateCounter () {
+  currentSpan.textContent = (i + 1).toString().padStart(2, '0')
+}
+
+
+
+
